6777538c49bf7ca330c2de09dbcf7f15bdaa7d1a
Michael Griebling <michael.griebling@solinst.com>
2015-02-03 07:34:28 -0500

    Initial Commit

---
+++ CalculatorSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorSwift
+//
+//  Created by Michael Griebling on 3Feb2015.
+//  Copyright (c) 2015 Solinst Canada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
