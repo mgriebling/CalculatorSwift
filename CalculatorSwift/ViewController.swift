81b883e62cdf1ea40776a6a2ca70c9c2d3a8aa53
Michael Griebling <michael.griebling@solinst.com>
2015-02-03 08:05:52 -0500

    Initial GUI layout

--- CalculatorSwift/ViewController.swift
+++ CalculatorSwift/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	@IBOutlet weak var display: UILabel!
+
+	@IBOutlet weak var stackDisplay: UILabel!
+
+	@IBAction func addDigit(sender: UIButton) {
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction func calculate(sender: UIButton) {
 	}
-
-
+	
+	@IBAction func addConstant(sender: UIButton) {
+	}
+	
+	@IBAction func clearState(sender: UIButton) {
+	}
 }
 
